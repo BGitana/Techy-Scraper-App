e59f61eb188119e35171d89d6a613be71aa2cd73
Gitana Brazlauskas <Bgitana@hotmail.com>
2020-01-27 23:46:56 -0500

    initial styles set up

---
+++ models/Headline.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+let HeadlineSchema = new Schema({
+
+    title: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    link: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    author: {
+        type: String,
+    },
+    authorLink: {
+        type: String,
+    },
+    imgSrc: {
+        type: String,
+    },
+    comment: {
+        type: Schema.Types.ObjectId,
+        ref: "Comment"
+    },
+    date: String, 
+        saved :{
+        type: Boolean,
+        default: false
+    }
+});
+
+let Headline = mongoose.model("Headline", HeadlineSchema)
+
+module.exports = Headline;
